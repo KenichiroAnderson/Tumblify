5d9dea05922816b24c32bb0df392cf5c371f808c
JacksonWilson99 <jwilso19@student.ubc.ca>
2024-04-11 13:43:43 -0700

    submit button fix v2

--- blog project/search-form.php
+++ blog project/search-form.php
@@ -45,7 +45,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
         <form action="search-results.php" method="get">
             <label for="searchQuery">Search:</label>
             <input type="text" id="searchQuery" name="search" required>
-            <button type="submit" onClick="goToResults()">Search</button>
+            <button type="submit" onclick="goToResults()">Search</button>
             <button type="button" onclick="goBackToHome()">X</button>
         </form>
     </main>
